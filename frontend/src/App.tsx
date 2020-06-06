2897e7caced106c92b505a3e4320960ed924bfef
Aldair Rodrigues <ifsulaldair@gmail.com>
2020-06-05 23:06:17 -0300

    fix functions deleted

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import Header from './Header'
+import Home from './pages/Home'
 
 function App() {
-  const [counter, setCounter] = useState(0) 
-
-  function handleButtonClick(){
-    setCounter(counter + 1)
-
-  }
   return (  
     <div>   
-      <Header title={`Contado: ${counter}`}/> 
-      <Header title="Ecoleta"/> 
-        <h1>{ counter }</h1>
-        
-        <button type="button" onClick={handleButtonClick}>Botão</button>
+      <Home />
     </div>
   );
 }
