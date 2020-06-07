66e7505df36753c047be1b59e3cc0b8a22753c81
Aldair Rodrigues <ifsulaldair@gmail.com>
2020-06-06 23:26:54 -0300

    fix/ Consuming IBGE API

--- frontend/src/pages/CreatePoint/index.tsx
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -1,27 +1,71 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, ChangeEvent } from 'react'
 import { Link } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
 import { Map, TileLayer, Marker} from 'react-leaflet'
 import api from '../../services/api'
+import axios from 'axios'
 
 import './styles.css'
 import logo from '../../assets/logo.svg'
 
 interface item {
-  id: number,
-  title: string,
-  image_url: string
+  id: number;
+  title: string;
+  image_url: string;
+}
+
+interface IBGEUFResponse {
+  sigla: string;
+}
+
+interface IBGEcityResponse {
+  nome: string;
 }
 
 const CreatePoint = () => {
 
   const [items, setItems] = useState<item[]>([])
+  const [ufs, setUfs] = useState<string[]>([])
+  const [selectedUf, setSelectedUf] = useState('0')
+  const [cities, setCities] = useState<string[]>([])
 
   useEffect(() => {
     api.get('/items').then(response => {
+
       setItems(response.data)
     })
   }, [])
+ 
+  useEffect(() => {
+    axios.get<IBGEUFResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados/')
+    .then(response => {
+      const ufInitials = response.data.map(uf => uf.sigla)
+
+      setUfs(ufInitials)
+      console.log(ufInitials)
+    })
+  }, [])
+
+  useEffect(() => {
+   if(selectedUf === '0'){
+     return;
+   }
+
+   axios.get<IBGEcityResponse[]>(
+    `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios/`)
+    .then((response) => {
+      const cityNames = response.data.map((city) => city.nome)
+      
+      setCities(cityNames)
+      console.log(cityNames)
+    })
+  }, [selectedUf])
+
+  function handleSelectUf(event: ChangeEvent<HTMLSelectElement>) {
+    const Uf = event.target.value
+    console.log(Uf)
+    setSelectedUf(Uf)
+  }
 
   return (
     <div id="page-create-point">
@@ -85,23 +129,43 @@ const CreatePoint = () => {
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
              <Marker position={[-31.7337873, -52.3223392]} />
-          </Map>
-
-         
+          </Map>    
 
           <div className="field-group">
 
             <div className="field">
               <label htmlFor="uf">Estado (UF)</label>
-              <select name="uf" id="uf">
-                <option value="0">Selecione uma UF</option>
+              <select 
+                name="uf" 
+                id="uf" 
+                value={selectedUf} 
+                onChange={handleSelectUf}
+              >
+                  <option value="0">Selecione um estado (UF)</option>
+                {ufs.map((uf) => (
+                  <option key={uf} value={uf}>
+                    {uf}
+                  </option>
+                    )
+                  )
+                }
               </select>
             </div>
 
             <div className="field">
               <label htmlFor="city">Cidade</label>
-              <select name="city" id="city">
-                <option value="city">Selecione uma cidade</option>
+              <select 
+                name="city" 
+                id="city"
+              >
+                <option value="0">Selecione uma cidade</option>
+                {cities.map((city) => (
+                   <option key={city} value={city}>
+                     {city}
+                    </option>
+                    )
+                  )
+                }
               </select>
             </div> 
 
@@ -114,12 +178,12 @@ const CreatePoint = () => {
           </legend>
 
           <ul className="items-grid">
-          {items.map(items =>  
-            <li>
-              <img src={items.image_url} alt=""/>
-          <span>{items.title}</span>
-            </li>)}
-           
+          {items.map((item) =>  
+            <li key={item.id} className="selected">
+              <img src={item.image_url} alt=""/>
+                <span>{item.title}</span>
+            </li>)
+          } 
           </ul>
 
         </fieldset>
